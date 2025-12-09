b19d5776d651e6dcdf2f4de40dbc27e774eede87
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2025-12-09 16:32:34 +0700

    refactor: remove unused Anime and Episode structs from model

--- internal/anime/model.go
+++ internal/anime/model.go
@@ -1,23 +1,5 @@
 package anime
 
-type Anime struct {
-    Title       string   `json:"title"`
-    Slug        string   `json:"slug"`
-    Synopsis    string   `json:"synopsis"`
-    PosterImage string   `json:"posterImage"`
-    Genres      []string `json:"genres"`
-    Status      string   `json:"status"`
-    Year        int      `json:"year"`
-    Season      string   `json:"season"`
-}
-
-type Episode struct {
-    Number    int    `json:"number"`
-    Title     string `json:"title"`
-    Synopsis  string `json:"synopsis"`
-    VideoURL  string `json:"videoUrl"` // link video yg kamu kirim ke client
-}
-
 type OngoingAnime struct {
 	Title       string `json:"title"`
 	Episode     int    `json:"episode"`
