dcef22ed37c46c16feb572299535fbd6916d1282
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2025-12-23 07:35:59 +0700

    fix(model): correct comment wording for day field in OngoingAnime struct

--- internal/anime/model.go
+++ internal/anime/model.go
@@ -3,7 +3,7 @@ package anime
 type OngoingAnime struct {
 	Title    string `json:"title"`
 	Episode  int    `json:"episode"`
-	Day      string `json:"day"`  // Sabtu, Jumat, dll
+	Day      string `json:"day"`  // Sabtu, Jumat, etc
 	Date     string `json:"date"` // "06 Des"
 	Image    string `json:"image"`
 	Endpoint string `json:"endpoint"`
